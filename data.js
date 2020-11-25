93e75b619fa6e8ae79a7d32a882a22ebf003c3c8
giomadeira <giomadeirag@gmail.com>
2020-11-25 18:06:29 -0300

    declaraçao das variaveis para interaçao na pagina

--- data.js
+++ data.js
@@ -1,9 +1,89 @@
+// manipulação de arrays e objetos
+
+
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+//export const example = () => {
+//  return 'example';
+//};
+
+//export const anotherExample = () => {
+//  return 'OMG';
+//};
+
+
+export const filtroNome = (rickandmorty) =>{
+  let arrayVazia = [];
+
+  rickandmorty.array.forEach((positionCharacter => {
+   let selectCharacter = (positionCharacter.name.find(
+
+    (character) => character == name
+    ));
+    if (selectCharacter != undefined){
+      arrayVazia.push(positionCharacter);
+    }
+  }));
+  console.log (arrayVazia)
+    return arrayVazia;
+
+};
+export const filtroEspecie = (rickandmorty) =>{
+  let arrayVazia = [];
+
+  rickandmorty.array.forEach((positionCharacter => {
+   let selectCharacter = (positionCharacter.species.find(
+
+    (character) => character == species
+    ));
+    if (selectCharacter != undefined){
+      arrayVazia.push(positionCharacter);
+    }
+  }));
+  console.log (arrayVazia)
+    return arrayVazia;
+
+};
+export const filtroGenero = (rickandmorty) =>{
+  let arrayVazia = [];
+
+  rickandmorty.array.forEach((positionCharacter => {
+   let selectCharacter = (positionCharacter.gender.find(
+
+    (character) => character == gender
+    ));
+    if (selectCharacter != undefined){
+      arrayVazia.push(positionCharacter);
+    }
+  }));
+  console.log (arrayVazia)
+    return arrayVazia;
+
+};
+export const filtroStatus = (rickandmorty) =>{
+  let arrayVazia = [];
+
+  rickandmorty.array.forEach((positionCharacter => {
+   let selectCharacter = (positionCharacter.status.find(
+
+    (character) => character == status
+    ));
+    if (selectCharacter != undefined){
+      arrayVazia.push(positionCharacter);
+    }
+  }));
+  console.log (arrayVazia)
+    return arrayVazia;
+
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+
+//const capturarDados = data.rickandmorty;
+//function personagens(){
+  //for (let i = 0; i < rickandmorty.length; i++){
+    
+  //}
+
+
+//}
--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
+// toda a interação dos códigos exibidos na tela - dom
+// eventListener ou eventHand
+
+import {filtroNome, filtroEspecie, filtroGenero,filtroStatus} from './data.js';
+import rickandmorty from './data/rickandmorty/rickandmorty.js';
+
  import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+
+//const nameCharacter = document.querySelector
+console.log (data)
+
+//for (persona of rickandmorty){
+  //  console.log(name)
+//}
+
+const namePersonagem= document.querySelector("#name");
+const statusDeVida = document.querySelector("#status");
+const especie = document.querySelector("#specie");
+const tipo = document.querySelector("#type");
+const genero = document.querySelector("#gender");
+const localizacao = document.querySelector(".location-name");
+const episodioAparece = document.querySelector("#episode");
+
+
