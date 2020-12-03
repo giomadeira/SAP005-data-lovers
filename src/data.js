c1e94de34c433df74151c9c47c653cac812faab2
Dunia <dunia07@hotmail.com>
2020-12-02 22:45:22 -0300

    refactor(layout): layout responsivo

--- src/data.js
+++ src/data.js
@@ -10,26 +10,26 @@
 //  return 'OMG';
 //};
 
-export const filtroEspecie = (data,userText) =>{
-    const filterEspecie = data.filter(index => index.species.includes(userText))
-    return (filterEspecie)
-  };
-
-//                           dados - texto do usuario
-export const filtroGenero = (data,userText ) =>{
-    const filterGenero = data.filter(index => index.gender.includes(userText))
-    return(filterGenero)
-  };
-  
-export const filtroStatus = (data,userText) => {
-  const filterStatus = data.filter(data => data.status.includes(userText)) 
-    return(filterStatus)
+export const filtroEspecie=(data,userText) => {
+  const filterEspecie=data.filter(index => index.species.includes(userText))
+   return(filterEspecie)
 };
 
-export const ordenacao = (data, order) => {
+//                           dados - texto do usuario
+export const filtroGenero=(data,userText ) => {
+  const filterGenero=data.filter(index => index.gender.includes(userText))
+  return(filterGenero)
+};
+  
+export const filtroStatus=(data,userText) => {
+  const filterStatus=data.filter(data => data.status.includes(userText)) 
+  return(filterStatus)
+};
+
+export const ordenacao=(data, order) => {
   return data.sort((a, b) => {
     if (order == "asc") {
-            if (a[order] > b[order]) {
+      if (a[order] > b[order]) {
         return 1;
       } else{
         return -1;
@@ -51,6 +51,6 @@ export const ordenacao = (data, order) => {
     
   //}
 
-export const filtroNome = (data, name) =>
-    data.filter(search => search.name.toUpperCase().indexOf(name.toUpperCase() >- 1));
+export const filtroNome=(data, name) =>
+  data.filter(search => search.name.toUpperCase().indexOf(name.toUpperCase() >- 1));
 
--- src/main.js
+++ src/main.js
@@ -2,75 +2,78 @@
 // eventListener ou eventHand
 
 
-import {filtroGenero,filtroStatus,filtroEspecie,filtroNome} from './data.js';
+import {filtroGenero, filtroStatus, filtroEspecie, filtroNome} from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
 
-//função para limpar cards
+// função para limpar cards
 
 // function clearCards() {
-//   document.getElementById("item-all-cards").innerHTML = ""
+// document.getElementById("item-all-cards").innerHTML = ""
 // }
 
-import {filterStatus} from './data.js';
-import data from './data/rickandmorty/rickandmorty.js';
-
-//para mostrar cards na tela chamando os elementos direto na função
-function mostrarCards (data) {
-  document.getElementById ("listagem").innerHTML = data.map((elemento) => `
-  <div class= "card-informa">
-  <div class = "card-frente">
-  <img src = "${elemento.image}">
-    <p><strong> Nome: ${elemento.name}</strong></p>
-</div> 
-<div class= "card-verso">
-  <ul class = "info-por-valor">
-  <li> Nome: ${elemento.name}</li>
-  <li> Status de vida: ${elemento.status}</li>
-  <li> Espécie: ${elemento.species}</li>
-  <li> Tipo: ${elemento.type}</li> 
-  <li> Gênero: ${elemento.gender}</li>
-  <li> Localização: ${elemento.location.name} </li>
-</div>
-`)
+// para mostrar cards na tela chamando os elementos direto na função
+function mostrarCards(data) {
+  document.getElementById("listagem").innerHTML=data.map((elemento) => `
+    <div class="card"> 
+      <div class="card-informa">
+        <div class="card-frente">
+          <img src="${elemento.image}">
+          <p><strong> Nome: ${elemento.name}</strong></p>
+        </div> 
+        <div class="card-verso">
+          <ul class="info-por-valor">
+          <li> Nome: ${elemento.name}</li>
+          <li> Status de vida: ${elemento.status}</li>
+          <li> Espécie: ${elemento.species}</li>
+          <li> Tipo: ${elemento.type}</li> 
+          <li> Gênero: ${elemento.gender}</li>
+          <li> Localização: ${elemento.location.name}</li>
+        </div>
+      </div>
+    </div>
+  `)
 }
 mostrarCards(data.results);
 
 
-function apresentaGenero(){
-let seletorGenero=document.getElementById("gender").value;
-// console.log (seletorGenero)
-let generoEscolhido = filtroGenero(data.results,seletorGenero);
-// mostrarCalculo.style.display= "flex";
-// mostrarCalculo.innerHTML=  `Temos ${contagemPorTipo(data.results, "gender", generoSelecionar.value)} personagens neste filtro.`
-return mostrarCards(generoEscolhido);
+function apresentaGenero() {
+  let seletorGenero=document.getElementById("gender").value;
+  // console.log (seletorGenero)
+  let generoEscolhido=filtroGenero(data.results,seletorGenero);
+  // mostrarCalculo.style.display= "flex";
+  // mostrarCalculo.innerHTML=  `Temos ${contagemPorTipo(data.results, "gender", generoSelecionar.value)} personagens neste filtro.`
+  return mostrarCards(generoEscolhido);
 }
+
 mostrarCards(data.results);
 document.getElementById("gender").addEventListener("change", apresentaGenero);
 
-function apresentaStatus(){
-let seletorStatus=document.getElementById("status").value;
-let statusEscolhido = filtroStatus(data.results,seletorStatus);
-return mostrarCards(statusEscolhido);
+function apresentaStatus() {
+  let seletorStatus=document.getElementById("status").value;
+  let statusEscolhido=filtroStatus(data.results,seletorStatus);
+  return mostrarCards(statusEscolhido);
 }
+
 mostrarCards(data.results);
 document.getElementById("status").addEventListener("change", apresentaStatus);
 
 
-function apresentaEspecie(){
-const seletorEspecie=document.getElementById("species").value;
-const especieEscolhido=filtroEspecie(data.results,seletorEspecie);
-return mostrarCards(especieEscolhido);
+function apresentaEspecie() {
+  const seletorEspecie=document.getElementById("species").value;
+  const especieEscolhido=filtroEspecie(data.results,seletorEspecie);
+  return mostrarCards(especieEscolhido);
 }
+
 mostrarCards(data.results);
 document.getElementById("species").addEventListener("change", apresentaEspecie);
  
 
-function apresentaNome(){
- const seletorNome = document.getElementById("name-search").value;
- const nomeEscolhido = filtroNome(data.results, seletorNome);
- return mostrarCards(nomeEscolhido);
- }
+function apresentaNome() {
+  const seletorNome=document.getElementById("name-search").value;
+  const nomeEscolhido=filtroNome(data.results, seletorNome);
+  return mostrarCards(nomeEscolhido);
+}
 
 // mostrarCards(data.results);
 // document.getElementById("btn-name").onclick.addEventListener("click",apresentaNome));
